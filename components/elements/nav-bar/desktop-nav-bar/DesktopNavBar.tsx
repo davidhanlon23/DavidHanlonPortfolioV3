f54a9c1be71a0bd05703fb9ddaac42e73bf2fa82
David M. Hanlon <davidhanlon23@gmail.com>
2024-10-31 14:49:05 -0400

    Update DesktopNavBar.tsx

--- components/elements/nav-bar/desktop-nav-bar/DesktopNavBar.tsx
+++ components/elements/nav-bar/desktop-nav-bar/DesktopNavBar.tsx
@@ -4,14 +4,17 @@ import { DarkModeToggle, RandomThemeButton } from '@/elements';
 const DesktopNavBar = () => {
     return(
         <nav className="ml-auto gap-4 sm:gap-6 items-center hidden md:flex">
-          <Link className="text-sm font-medium hover:underline underline-offset-4" href="/#features">
-            Features
+          <Link className="text-sm font-medium hover:underline underline-offset-4" href="/#projects">
+            Projects
           </Link>
-          <Link className="text-sm font-medium hover:underline underline-offset-4" href="/sign-in">
-            Sign In
+          <Link className="text-sm font-medium hover:underline underline-offset-4" href="/#experience">
+            Experience
           </Link>
-          <Link className="text-sm font-medium hover:underline underline-offset-4" href="/sign-up">
-            Sign Up
+          <Link className="text-sm font-medium hover:underline underline-offset-4" href="/#education">
+            Education
+          </Link>
+          <Link className="text-sm font-medium hover:underline underline-offset-4" href="/#contact">
+            Contact
           </Link>
           <DarkModeToggle />
           <RandomThemeButton />
